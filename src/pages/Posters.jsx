f14428f92e400e4dcc2f8e7ac1fef9e82df0e749
Emese Elkind <emese.elkind@gmail.com>
2025-01-06 00:11:08 -0500

    media

--- src/pages/Posters.jsx
+++ src/pages/Posters.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./Poster.css"; // Use a relative path here
+import "./Poster.css";
 
 import Poster1 from "C:/Users/emese/Desktop/Emese's Art/emeses-art/public/posters/PXL_20250102_001656830~2.jpg";
 import Poster2 from "C:/Users/emese/Desktop/Emese's Art/emeses-art/public/posters/PXL_20250102_001735674.jpg";
@@ -12,12 +12,14 @@ const imageDetails = [
 const PostersPage = () => {
   return (
     <div className="posters-page">
-      <h1 className="title">Posters Gallery</h1>
-      <p className="description">   </p>
+      <h1 className="title">Posters </h1>
+      <p className="description"></p>
       <div className="posters-row">
         {imageDetails.map((poster, index) => (
           <div className="poster" key={index}>
-            <img src={poster.src} alt={poster.title} className="poster-image"  />
+            <div className="poster-image-container">
+              <img src={poster.src} alt={poster.title} className="poster-image" />
+            </div>
             <p className="poster-title">{poster.title}</p>
           </div>
         ))}
@@ -25,4 +27,5 @@ const PostersPage = () => {
     </div>
   );
 };
+
 export default PostersPage;
